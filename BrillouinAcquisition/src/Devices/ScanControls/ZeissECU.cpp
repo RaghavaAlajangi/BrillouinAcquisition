de9ed66908e234bbeb0df6c55e3b692a8561ac36
Labadmin Guck <labadmin-guck@mpl.mpg.de>
2025-08-13 16:01:48 +0200

    remove SCAN_EOMCALIBRATION member

--- BrillouinAcquisition/src/Devices/ScanControls/ZeissECU.cpp
+++ BrillouinAcquisition/src/Devices/ScanControls/ZeissECU.cpp
@@ -30,8 +30,7 @@ ZeissECU::ZeissECU() noexcept {
 		{ "Fluo Blue",		ScanPreset::SCAN_EPIFLUOBLUE,	{ {1}, {}, {2}, {3},  {}, {2}, {2}, {1}, {} }	},	// Fluorescence blue
 		{ "Fluo Green",		ScanPreset::SCAN_EPIFLUOGREEN,	{ {1}, {}, {3}, {3},  {}, {2}, {2}, {1}, {} }	},	// Fluorescence green
 		{ "Fluo Red",		ScanPreset::SCAN_EPIFLUORED,	{ {1}, {}, {4}, {3},  {}, {2}, {2}, {1}, {} }	},	// Fluorescence red
-		{ "Laser off",		ScanPreset::SCAN_LASEROFF,		{ {1}, {},  {},  {},  {},  {}, {1},  {}, {} }	},	// Laser off
-		{ "EOM",            ScanPreset::SCAN_EOMCALIBRATION,{ {2}, {}, {1}, {3}, {1}, {2}, {1},  {}, {} }	}	// EOM Calibration
+		{ "Laser off",		ScanPreset::SCAN_LASEROFF,		{ {1}, {},  {},  {},  {},  {}, {1},  {}, {} }	}	// Laser off
 
 	};
 
--- BrillouinAcquisition/src/Devices/ScanControls/ZeissMTB_Erlangen.cpp
+++ BrillouinAcquisition/src/Devices/ScanControls/ZeissMTB_Erlangen.cpp
@@ -11,10 +11,10 @@ ZeissMTB_Erlangen::ZeissMTB_Erlangen() noexcept {
 		DeviceElement { "Beam Block",			2, (int)DEVICE_ELEMENT::BEAMBLOCK,	{ "Close", "Open" } },
 		DeviceElement { "Objective",			6, (int)DEVICE_ELEMENT::OBJECTIVE },
 		DeviceElement { "Reflector",			6, (int)DEVICE_ELEMENT::REFLECTOR,	{ "Green", "Red", "Blue" } },
-		DeviceElement { "Sideport",			3, (int)DEVICE_ELEMENT::SIDEPORT,	{ "Eyepiece", "Left", "Right" } },
+		DeviceElement { "Sideport",			    3, (int)DEVICE_ELEMENT::SIDEPORT,	{ "Eyepiece", "Left", "Right" } },
 		DeviceElement { "RL Shutter",			2, (int)DEVICE_ELEMENT::RLSHUTTER,	{ "Close", "Open" } },
 		DeviceElement { "Mirror",				2, (int)DEVICE_ELEMENT::MIRROR,		{ "ODT", "Brillouin" } },
-		DeviceElement { "LED illumination",	2, (int)DEVICE_ELEMENT::LEDLAMP,	{ "Off", "On" } }
+		DeviceElement { "LED illumination",	    2, (int)DEVICE_ELEMENT::LEDLAMP,	{ "Off", "On" } }
 	};
 
 	m_presets = {
@@ -25,8 +25,7 @@ ZeissMTB_Erlangen::ZeissMTB_Erlangen() noexcept {
 		{ "Eyepiece",		ScanPreset::SCAN_EYEPIECE,		{ {1}, {}, {4}, {1}, {1}, {2}, {2} }	},	// Eyepiece
 		{ "Fluo Blue",		ScanPreset::SCAN_EPIFLUOBLUE,	{ {1}, {}, {3},  {}, {2}, {1}, {1} }	},	// Fluorescence blue
 		{ "Fluo Green",		ScanPreset::SCAN_EPIFLUOGREEN,	{ {1}, {}, {1},  {}, {2}, {1}, {1} }	},	// Fluorescence green
-		{ "Fluo Red",		ScanPreset::SCAN_EPIFLUORED,	{ {1}, {}, {2},  {}, {2}, {1}, {1} }	},	// Fluorescence red
-		{ "EOM",            ScanPreset::SCAN_EOMCALIBRATION,{ {2}, {}, {4}, {2}, {1}, {2}, {2} }	}	// EOM Calibration
+		{ "Fluo Red",		ScanPreset::SCAN_EPIFLUORED,	{ {1}, {}, {2},  {}, {2}, {1}, {1} }	}	// Fluorescence red
 	};
 
 	m_elementPositions = std::vector<double>((int)DEVICE_ELEMENT::COUNT, -1);
