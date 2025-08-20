114a4410c6d335323fcb6be9278031ceb12706ef
RaghavaAlajangi <raghava.alajangi95@gmail.com>
2025-08-20 11:50:44 +0200

    Update polynomial coeffs and refactor code

--- BrillouinAcquisition/src/Devices/EOM.cpp
+++ BrillouinAcquisition/src/Devices/EOM.cpp
@@ -1,35 +1,51 @@
 #include "stdafx.h" 
 #include "EOM.h"
 #include <cmath>
-#include <NIDAQmx.h>
+#include <iostream>
 
 double EOM::frequencyToVoltage(double frequency)
 {
     // Polynomial coefficients
-    double p1 = 0.002255, p2 = -0.1066, p3 = 1.786, p4 = -13.79, p5 = 53.31, p6 = -81.53;
+    double p0 = -4.19117e-6;
+    double p1 = 0.000168553;
+    double p2 = 0.00273963;
+    double p3 = 0.0228064;
+    double p4 = -0.106149;
+    double p5 = 0.596645;
+    double p6 = 3.40049;
 
-    double voltage = (p1 * pow(frequency, 5)) +
-                     (p2 * pow(frequency, 4)) +
-                     (p3 * pow(frequency, 3)) +
-                     (p4 * pow(frequency, 2)) +
+    // Polynomial (degree 6)
+    double voltage = (p0 * std::pow(frequency, 6)) +
+                     (p1 * std::pow(frequency, 5)) +
+                     (p2 * std::pow(frequency, 4)) +
+                     (p3 * std::pow(frequency, 3)) +
+                     (p4 * std::pow(frequency, 2)) +
                      (p5 * frequency) +
                      p6;
 
     return std::round(voltage * 100.0) / 100.0;
 }
 
-void EOM::writeVoltageToDAQ(double voltage)
-{
+void EOM::writeVoltage(const std::string& channel, double voltage) {
     TaskHandle taskHandle = nullptr;
 
-    // Create and configure DAQ task
-    DAQmxCreateTask("", &taskHandle);
-    DAQmxCreateAOVoltageChan(taskHandle, "Dev2/ao0", "", 0.0, 10.0, DAQmx_Val_Volts, nullptr);
+    int32 error = DAQmxCreateTask("", &taskHandle);
+    if (error) { std::cerr << "Error creating task: " << error << std::endl; return; }
 
-    // Write voltage
-    DAQmxWriteAnalogScalarF64(taskHandle, true, 10.0, voltage, nullptr);
+    error = DAQmxCreateAOVoltageChan(taskHandle, channel.c_str(), "", 0.0, 10.0, DAQmx_Val_Volts, nullptr);
+    if (error) { std::cerr << "Error creating channel: " << error << std::endl; DAQmxClearTask(taskHandle); return; }
 
-    // Cleanup
+    error = DAQmxWriteAnalogScalarF64(taskHandle, true, 10.0, voltage, nullptr);
+    if (error) { std::cerr << "Error writing voltage: " << error << std::endl; }
+
     DAQmxStopTask(taskHandle);
     DAQmxClearTask(taskHandle);
+}
+
+void EOM::writeVoltageToDAQ(double voltage) {
+    writeVoltage("Dev2/ao0", voltage);
+}
+
+void EOM::writeAttenuationVoltageToDAQ(double voltage) {
+    writeVoltage("Dev2/ao1", voltage);
 }
--- BrillouinAcquisition/src/Devices/EOM.h
+++ BrillouinAcquisition/src/Devices/EOM.h
@@ -1,10 +1,15 @@
 #ifndef EOM_H
 #define EOM_H
 
+#include <NIDAQmx.h>
+
 class EOM {
 public:
-    static double frequencyToVoltage(double frequency);
     static void writeVoltageToDAQ(double voltage);
+    static void writeAttenuationVoltageToDAQ(double voltage);
+    static double frequencyToVoltage(double frequency);
 };
+private:
+    static void writeVoltage(const std::string& channel, double voltage);
 
 #endif // EOM_H
